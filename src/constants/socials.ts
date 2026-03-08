8f137b48fa11fb7f21a8f9e82d99609be57609f4
Iva Kobalava <ivakobalava@gmail.com>
2026-03-09 02:45:17 +0400

    Add Contact window with social links, email display, and responsive styling

---
+++ src/constants/socials.ts
@@ -0,0 +1,32 @@
+const socials = [
+    {
+        id: 1,
+        text: 'Github',
+        icon: '/icons/github.svg',
+        bg: '#f4656b',
+        link: 'https://github.com/ivaiva89',
+    },
+    {
+        id: 2,
+        text: 'Platform',
+        icon: '/icons/atom.svg',
+        bg: '#4bcb63',
+        link: 'https://ivakobalava.dev/',
+    },
+    {
+        id: 3,
+        text: 'Twitter/X',
+        icon: '/icons/twitter.svg',
+        bg: '#ff866b',
+        link: 'https://x.com/ikobalava',
+    },
+    {
+        id: 4,
+        text: 'LinkedIn',
+        icon: '/icons/linkedin.svg',
+        bg: '#05b6f6',
+        link: 'https://www.linkedin.com/in/iveri-kobalava/',
+    },
+]
+
+export { socials }
